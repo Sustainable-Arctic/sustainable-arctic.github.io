65535773584b41fc35d70cd645a8f9102145c1a1
Max Karel <maxdkarel@gmail.com>
2021-11-06 17:32:00 +0100

    localize

--- index.js
+++ index.js
@@ -55,6 +55,39 @@ var jsonFile = {
         }
     ]
 }  
+
+var jsonBuildings = {
+    "parameters": [
+        {
+            "temp":
+            {
+                "min": -200,
+                "max": 1000,
+                "mean": 500
+            },
+            "speed":
+            {
+                "min": -300,
+                "max": 2000,
+                "mean": 800
+            }   
+        }, 
+        {
+            "temp":
+            {
+                "min": -200,
+                "max": 1000,
+                "mean": 500
+            },
+            "speed":
+            {
+                "min": -300,
+                "max": 2000,
+                "mean": 800
+            }
+        }
+    ]
+}  
 class Hex {
 
     constructor(center_x, center_y) 
@@ -77,10 +110,10 @@ class Hex {
     draw() {
         const bermudaTriangle = new google.maps.Polygon({
             paths: this.triangleCoords,
-            strokeColor: "#FF0000",
+            strokeColor: "#111111",
             strokeOpacity: 0.8,
             strokeWeight: 3,
-            fillColor: "#FF0000",
+            fillColor: "#111111",
             fillOpacity: 0.35,
         });
         return bermudaTriangle;
@@ -88,23 +121,40 @@ class Hex {
 
 }
 
+var googleHexes = [];
+
+function calculateDistances()
+{
+    var e = document.getElementById("useCaseSelector");
+    console.log("Selected use case" + e.value);
+
+
+
+    let distances = [];
+    for (let i = 0; i < googleHexes.length; i++) {
+
+    }
+
+}
+
+function redrawMap()
+{
+    var e = document.getElementById("useCaseSelector");
+    console.log("Selected use case" + e.value);
+
+    for (let i = 0; i < googleHexes.length; i++) {
+        googleHexes[i].setOptions({fillColor : 'red'});
+    }
+}
+
 function initMap() {
-
     map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 5,
-        center: { lat: 66.886, lng: 14.268 },
+        zoom: 6,
+        center: { lat: 69 - 0.420, lng: 26 },
         mapTypeId: "terrain",
+        gestureHandling: "none",
+        zoomControl: false,      
     });
-
-    // const map = new google.maps.Map(document.getElementById("map"), {
-    //     zoom: 30,
-    //     center: { lat: 0, lng: 0 },
-    //     mapTypeControl: true,
-    // });
-
-    // initGallPeters();
-    // map.mapTypes.set("gallPeters", gallPetersMapType);
-    // map.setMapTypeId("gallPeters");
 
     let hexes = jsonFile.data;
     let width = 0.25;
@@ -120,80 +170,21 @@ function initMap() {
             let hex = new Hex(17.0 + delta_x, 70.0 - delta_y);
             googleHex = hex.draw();
             googleHex.setMap(map);
-            // Add a listener for the click event.
             googleHex.set("hex", hexes[0]);
             googleHex.addListener("click", showArrays);  
+            googleHexes.push(googleHex);
         }
     }
 
-  
     infoWindow = new google.maps.InfoWindow();
-}
-
-
-let gallPetersMapType;
-
-function initGallPeters() {
-  const GALL_PETERS_RANGE_X = 800;
-  const GALL_PETERS_RANGE_Y = 512;
-
-  // Fetch Gall-Peters tiles stored locally on our server.
-  gallPetersMapType = new google.maps.ImageMapType({
-    getTileUrl: function (coord, zoom) {
-      const scale = 1 << zoom;
-      // Wrap tiles horizontally.
-      const x = ((coord.x % scale) + scale) % scale;
-      // Don't wrap tiles vertically.
-      const y = coord.y;
-
-      if (y < 0 || y >= scale) return "";
-      return (
-        "https://developers.google.com/maps/documentation/" +
-        "javascript/examples/full/images/gall-peters_" +
-        zoom +
-        "_" +
-        x +
-        "_" +
-        y +
-        ".png"
-      );
-    },
-    tileSize: new google.maps.Size(GALL_PETERS_RANGE_X, GALL_PETERS_RANGE_Y),
-    minZoom: 0,
-    maxZoom: 2,
-    name: "Gall-Peters",
-  });
-  // Describe the Gall-Peters projection used by these tiles.
-  gallPetersMapType.projection = {
-    fromLatLngToPoint: function (latLng) {
-      const latRadians = (latLng.lat() * Math.PI) / 180;
-      return new google.maps.Point(
-        GALL_PETERS_RANGE_X * (0.5 + latLng.lng() / 360),
-        GALL_PETERS_RANGE_Y * (0.5 - 0.5 * Math.sin(latRadians))
-      );
-    },
-    fromPointToLatLng: function (point, noWrap) {
-      const x = point.x / GALL_PETERS_RANGE_X;
-      const y = Math.max(0, Math.min(1, point.y / GALL_PETERS_RANGE_Y));
-      return new google.maps.LatLng(
-        (Math.asin(1 - 2 * y) * 180) / Math.PI,
-        -180 + 360 * x,
-        noWrap
-      );
-    },
-  };
 }
 
 function createPrintout(atribute, atributeName) {
     return "<br>" + atributeName + ": " + atribute.max + ", " +  atribute.min;
 }
 
-
 function showArrays(event) {
-  // Since this polygon has only one path, we can call getPath() to return the
-  // MVCArray of LatLngs.
   const polygon = this;
-  const vertices = polygon.getPath();
 
   let contentString =
     "<b>Hex</b><br>" +
@@ -209,7 +200,6 @@ function showArrays(event) {
   contentString += createPrintout(mhex.speed, "Average year wind speed");
   contentString += '<br><img src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/google/110/heavy-check-mark_2714.png" alt="Italian Trulli">'
 
-  // Replace the info window's content and position.
   infoWindow.setContent(contentString);
   infoWindow.setPosition(event.latLng);
   infoWindow.open(map);
